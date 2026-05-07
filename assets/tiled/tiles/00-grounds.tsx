b36edf43abbd80e2e5f043f0bc4715252d66bafc
adrien-parasote <adfien.parasote@gmail.com>
2026-05-07 13:40:37 +0200

    assets save

--- assets/tiled/tiles/00-grounds.tsx
+++ assets/tiled/tiles/00-grounds.tsx
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="00-grounds" class="03-tileset_ground" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+<tileset version="1.10" tiledversion="1.12.1" name="00-grounds" class="03-tileset_ground" tilewidth="32" tileheight="32" tilecount="120" columns="10">
  <properties>
   <property name="type" value="ground"/>
  </properties>
- <image source="../../images/tilesets/00-grounds.png" width="256" height="256"/>
+ <image source="../../images/tilesets/00-grounds.png" width="320" height="384"/>
  <tile id="0" type="03-tileset_ground">
   <properties>
-   <property name="material" value="stone"/>
+   <property name="material" value="wood"/>
   </properties>
  </tile>
  <tile id="1" type="03-tileset_ground">
   <properties>
-   <property name="material" value="stone"/>
+   <property name="material" value="wood"/>
   </properties>
  </tile>
  <tile id="2" type="03-tileset_ground">
@@ -26,22 +26,34 @@
  </tile>
  <tile id="4" type="03-tileset_ground">
   <properties>
-   <property name="material" value="stone"/>
+   <property name="material" value="wood"/>
   </properties>
  </tile>
  <tile id="5" type="03-tileset_ground">
   <properties>
-   <property name="material" value="stone"/>
+   <property name="material" value="wood"/>
   </properties>
  </tile>
  <tile id="6" type="03-tileset_ground">
   <properties>
-   <property name="material" value="stone"/>
+   <property name="material" value="wood"/>
   </properties>
  </tile>
- <tile id="7" type="03-tileset_ground"/>
- <tile id="8" type="03-tileset_ground"/>
- <tile id="9" type="03-tileset_ground"/>
+ <tile id="7" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="8" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="9" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
  <tile id="10" type="03-tileset_ground">
   <properties>
    <property name="material" value="wood"/>
@@ -52,91 +64,446 @@
    <property name="material" value="wood"/>
   </properties>
  </tile>
- <tile id="12" type="03-tileset_ground"/>
- <tile id="13" type="03-tileset_ground"/>
- <tile id="14" type="03-tileset_ground"/>
- <tile id="15" type="03-tileset_ground"/>
- <tile id="16" type="03-tileset_ground"/>
- <tile id="17" type="03-tileset_ground"/>
- <tile id="18" type="03-tileset_ground"/>
- <tile id="19" type="03-tileset_ground"/>
- <tile id="20" type="03-tileset_ground"/>
- <tile id="21" type="03-tileset_ground"/>
- <tile id="22" type="03-tileset_ground"/>
- <tile id="23" type="03-tileset_ground"/>
- <tile id="24" type="03-tileset_ground"/>
- <tile id="25" type="03-tileset_ground"/>
- <tile id="26" type="03-tileset_ground"/>
- <tile id="27" type="03-tileset_ground"/>
- <tile id="28" type="03-tileset_ground"/>
- <tile id="29" type="03-tileset_ground"/>
- <tile id="30" type="03-tileset_ground"/>
- <tile id="31" type="03-tileset_ground"/>
- <tile id="32" type="03-tileset_ground"/>
- <tile id="33" type="03-tileset_ground"/>
- <tile id="34" type="03-tileset_ground"/>
- <tile id="35" type="03-tileset_ground"/>
- <tile id="36" type="03-tileset_ground"/>
- <tile id="37" type="03-tileset_ground"/>
- <tile id="38" type="03-tileset_ground"/>
- <tile id="39" type="03-tileset_ground"/>
- <tile id="40" type="03-tileset_ground"/>
- <tile id="41" type="03-tileset_ground"/>
- <tile id="42" type="03-tileset_ground"/>
- <tile id="43" type="03-tileset_ground"/>
- <tile id="44" type="03-tileset_ground"/>
- <tile id="45" type="03-tileset_ground"/>
- <tile id="46" type="03-tileset_ground"/>
- <tile id="47" type="03-tileset_ground"/>
+ <tile id="12" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="13" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="14" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="15" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="16" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="17" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="18" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="19" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="20" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="21" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="22" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="23" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="24" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="25" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="26" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="27" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="28" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="29" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="30" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="31" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="32" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="33" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="34" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="35" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="36" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="37" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="38" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="39" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="40" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="41" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="42" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="43" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="44" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="45" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="46" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="47" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
  <tile id="48" type="03-tileset_ground">
   <properties>
-   <property name="collidable" type="bool" value="false"/>
-   <property name="depth" type="int" value="0"/>
+   <property name="material" value="stone"/>
   </properties>
  </tile>
  <tile id="49" type="03-tileset_ground">
   <properties>
-   <property name="collidable" type="bool" value="false"/>
-   <property name="depth" type="int" value="0"/>
+   <property name="material" value="stone"/>
   </properties>
  </tile>
  <tile id="50" type="03-tileset_ground">
   <properties>
-   <property name="collidable" type="bool" value="false"/>
-   <property name="depth" type="int" value="0"/>
+   <property name="material" value="stone"/>
   </properties>
  </tile>
  <tile id="51" type="03-tileset_ground">
   <properties>
-   <property name="collidable" type="bool" value="false"/>
-   <property name="depth" type="int" value="0"/>
+   <property name="material" value="stone"/>
   </properties>
  </tile>
  <tile id="52" type="03-tileset_ground">
   <properties>
-   <property name="collidable" type="bool" value="false"/>
-   <property name="depth" type="int" value="0"/>
+   <property name="material" value="stone"/>
   </properties>
  </tile>
  <tile id="53" type="03-tileset_ground">
   <properties>
-   <property name="collidable" type="bool" value="false"/>
-   <property name="depth" type="int" value="0"/>
+   <property name="material" value="stone"/>
   </properties>
  </tile>
  <tile id="54" type="03-tileset_ground">
   <properties>
-   <property name="collidable" type="bool" value="false"/>
-   <property name="depth" type="int" value="0"/>
+   <property name="material" value="stone"/>
   </properties>
  </tile>
- <tile id="55" type="03-tileset_ground"/>
- <tile id="56" type="03-tileset_ground"/>
- <tile id="57" type="03-tileset_ground"/>
- <tile id="58" type="03-tileset_ground"/>
- <tile id="59" type="03-tileset_ground"/>
- <tile id="60" type="03-tileset_ground"/>
- <tile id="61" type="03-tileset_ground"/>
- <tile id="62" type="03-tileset_ground"/>
- <tile id="63" type="03-tileset_ground"/>
+ <tile id="55" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="56" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="57" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="58" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="59" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="60" type="03-tileset_ground">
+  <properties>
+   <property name="collidable" type="bool" value="false"/>
+   <property name="depth" type="int" value="0"/>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="61" type="03-tileset_ground">
+  <properties>
+   <property name="collidable" type="bool" value="false"/>
+   <property name="depth" type="int" value="0"/>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="62" type="03-tileset_ground">
+  <properties>
+   <property name="collidable" type="bool" value="false"/>
+   <property name="depth" type="int" value="0"/>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="63" type="03-tileset_ground">
+  <properties>
+   <property name="collidable" type="bool" value="false"/>
+   <property name="depth" type="int" value="0"/>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="64" type="03-tileset_ground">
+  <properties>
+   <property name="collidable" type="bool" value="false"/>
+   <property name="depth" type="int" value="0"/>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="65" type="03-tileset_ground">
+  <properties>
+   <property name="collidable" type="bool" value="false"/>
+   <property name="depth" type="int" value="0"/>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="66" type="03-tileset_ground">
+  <properties>
+   <property name="collidable" type="bool" value="false"/>
+   <property name="depth" type="int" value="0"/>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="67" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="68" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="69" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="70" type="03-tileset_ground"/>
+ <tile id="71" type="03-tileset_ground"/>
+ <tile id="72" type="03-tileset_ground"/>
+ <tile id="73" type="03-tileset_ground"/>
+ <tile id="74" type="03-tileset_ground"/>
+ <tile id="75" type="03-tileset_ground"/>
+ <tile id="76" type="03-tileset_ground"/>
+ <tile id="77" type="03-tileset_ground"/>
+ <tile id="78" type="03-tileset_ground"/>
+ <tile id="79" type="03-tileset_ground"/>
+ <tile id="80" type="03-tileset_ground"/>
+ <tile id="81" type="03-tileset_ground"/>
+ <tile id="82" type="03-tileset_ground"/>
+ <tile id="83" type="03-tileset_ground"/>
+ <tile id="84" type="03-tileset_ground"/>
+ <tile id="85" type="03-tileset_ground"/>
+ <tile id="86" type="03-tileset_ground"/>
+ <tile id="87" type="03-tileset_ground"/>
+ <tile id="88" type="03-tileset_ground"/>
+ <tile id="89" type="03-tileset_ground"/>
+ <tile id="90" type="03-tileset_ground"/>
+ <tile id="91" type="03-tileset_ground"/>
+ <tile id="92" type="03-tileset_ground"/>
+ <tile id="93" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="94" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="95" type="03-tileset_ground"/>
+ <tile id="96" type="03-tileset_ground"/>
+ <tile id="97" type="03-tileset_ground"/>
+ <tile id="98" type="03-tileset_ground"/>
+ <tile id="99" type="03-tileset_ground"/>
+ <tile id="100" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="101" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="102" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="103" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="104" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="105" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="106" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="107" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="108" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="109" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="110" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="111" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="112" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="113" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="114" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="115" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="116" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="117" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="118" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="119" type="03-tileset_ground">
+  <properties>
+   <property name="material" value="wood"/>
+  </properties>
+ </tile>
 </tileset>
