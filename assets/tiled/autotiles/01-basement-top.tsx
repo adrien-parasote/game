f34fb40dbec42656909692f1780f9ba4e89c2446
adrien-parasote <adrien.parasote@gmail.com>
2026-06-06 20:18:53 +0200

    game asset update

---
+++ assets/tiled/autotiles/01-basement-top.tsx
@@ -0,0 +1,294 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="01-basement-top" class="00-tileset" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <properties>
+  <property name="depth" type="int" value="2"/>
+ </properties>
+ <image source="../../images/autotiles/01-basement-top.png" width="256" height="192"/>
+ <tile id="0" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="6" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="7" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="9" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="10" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="11" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="12" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="13" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="14" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="15" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="16" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="17" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="18" type="00-tileset"/>
+ <tile id="19" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="20" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="21" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="22" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="23" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="24" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="25" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="26" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="27" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="28" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="29" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="30" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="31" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="32" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="33" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="34" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="35" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="36" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="37" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="38" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="39" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="40" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="41" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="42" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="43" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="44" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="45" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="46" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="47" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <wangsets>
+  <wangset name="01-basement-top" type="mixed" tile="-1">
+   <wangcolor name="01-basement-top" color="#cccccc" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="3" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="4" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="5" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="6" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="7" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="8" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="9" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="10" wangid="1,0,1,0,0,0,1,1"/>
+   <wangtile tileid="11" wangid="1,1,1,0,0,0,1,0"/>
+   <wangtile tileid="12" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="13" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="14" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="15" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="16" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="17" wangid="1,0,0,0,1,0,1,1"/>
+   <wangtile tileid="18" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="19" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="20" wangid="1,1,1,0,1,0,0,0"/>
+   <wangtile tileid="21" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="22" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="23" wangid="1,0,1,0,1,0,1,1"/>
+   <wangtile tileid="24" wangid="1,1,1,0,1,0,1,0"/>
+   <wangtile tileid="25" wangid="1,1,1,0,1,0,1,1"/>
+   <wangtile tileid="26" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="27" wangid="1,0,0,0,1,1,1,0"/>
+   <wangtile tileid="28" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="29" wangid="0,0,1,0,1,1,1,0"/>
+   <wangtile tileid="30" wangid="1,0,1,0,1,1,1,0"/>
+   <wangtile tileid="31" wangid="1,0,1,0,1,1,1,1"/>
+   <wangtile tileid="32" wangid="1,1,1,0,1,1,1,0"/>
+   <wangtile tileid="33" wangid="1,1,1,0,1,1,1,1"/>
+   <wangtile tileid="34" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="35" wangid="1,0,1,1,1,0,0,0"/>
+   <wangtile tileid="36" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="37" wangid="0,0,1,1,1,0,1,0"/>
+   <wangtile tileid="38" wangid="1,0,1,1,1,0,1,0"/>
+   <wangtile tileid="39" wangid="1,0,1,1,1,0,1,1"/>
+   <wangtile tileid="40" wangid="1,1,1,1,1,0,1,0"/>
+   <wangtile tileid="41" wangid="1,1,1,1,1,0,1,1"/>
+   <wangtile tileid="42" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="43" wangid="1,0,1,1,1,1,1,0"/>
+   <wangtile tileid="44" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="45" wangid="1,1,1,1,1,1,1,0"/>
+   <wangtile tileid="46" wangid="1,1,1,1,1,1,1,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
---
+++ assets/tiled/autotiles/01-basement-wall.tsx
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="01-basement-wall" class="00-tileset" tilewidth="32" tileheight="32" tilecount="16" columns="16">
+ <properties>
+  <property name="walkable" type="bool" value="false"/>
+ </properties>
+ <image source="../../images/autotiles/01-basement-wall.png" width="512" height="32"/>
+ <tile id="0" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="1" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="2" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="3" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="4" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="5" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="6" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="7" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="8" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="9" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="10" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="11" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="12" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="13" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="14" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="15" type="00-tileset">
+  <properties>
+   <property name="walkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <wangsets>
+  <wangset name="01-basement-wall" type="mixed" tile="-1">
+   <wangcolor name="01-basement-wall" color="#cccccc" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="1" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="2" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="3" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="4" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="5" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="6" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="7" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="8" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="9" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="10" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="11" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="12" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="13" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="14" wangid="0,0,0,0,0,0,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
